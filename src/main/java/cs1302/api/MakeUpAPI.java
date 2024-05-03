bab60b1f42a74b02961b00c480339ee8b35e5532
Ngoc Tuong Vi Tran <nt27959@uga.edu>
2024-05-03 06:28:53 -0400

    added API call and recommend product method

--- src/main/java/cs1302/api/MakeUpAPI.java
+++ src/main/java/cs1302/api/MakeUpAPI.java
@@ -2,7 +2,9 @@ package cs1302.api;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -79,80 +81,169 @@ public class MakeUpAPI {
     private static final String MASCARA_ENDPOINT =
             "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=mascara";
 
+    /**
+     * Retrieves makeup products from the specified API endpoint based on the given type.
+     *
+     * @param endpoint The API endpoint from which to retrieve makeup product data.
+     * @return An array of Makeup objects containing makeup product data
+     *         obtained from the specified endpoint.
+     * @throws IOException          If an I/O error occurs while fetching data from the API.
+     * @throws InterruptedException If the thread is interrupted while waiting for the API response.
+     */
     private static MakeUp[] getMakeUpProductByType(String endpoint)
             throws IOException, InterruptedException {
         String json = AccuWeather.fetchString(endpoint);
         return AccuWeather.GSON.fromJson(json, MakeUp[].class);
     } // getMakeUpProduct
 
-    private static List<MakeUp> productCheck(MakeUp[] product, String[][] tag)
+    /**
+     * Recommends makeup products based on the provided tag list.
+     *
+     * @param tagList An array of tags specifying the desired characteristics of makeup products
+     *                for each makeup type (blush, eyeliner, foundation, lipstick, mascara).
+     *                Each element in the array corresponds to a makeup type and
+     *                contains one or more tags.
+     *                If a tag is empty for a makeup type,
+     *                products for that type will be chosen randomly.
+     * @return A list of lists containing recommended makeup products for each makeup type.
+     * Each inner list corresponds to a makeup type (blush, eyeliner, foundation, lipstick, mascara)
+     * and contains the recommended makeup products of that type.
+     * @throws IOException          If an I/O error occurs while fetching makeup products.
+     * @throws InterruptedException If the thread is interrupted while
+     *                              waiting for the operation to complete.
+     */
+    private static List<List<MakeUp>> recommendMakeUp(String[] tagList)
             throws IOException, InterruptedException {
-        List<MakeUp[]> products = new ArrayList<>();
-        products.add(getMakeUpProductByType(BLUSH_ENDPOINT));
-        products.add(getMakeUpProductByType(EYELINER_ENDPOINT));
-        products.add(getMakeUpProductByType(FOUNDATION_ENDPOINT));
-        products.add(getMakeUpProductByType(LIPSTICK_ENDPOINT));
-        products.add(getMakeUpProductByType(MASCARA_ENDPOINT));
 
-        for (MakeUp p : product) {
-            return products;
-        }
+        // list of makeup list contains makeup products sorts by product type
+        // (blush, eyeliner, foundation, lipstick, mascara)
+        List<List<MakeUp>> products = new ArrayList<>();
+        products.add(Arrays.asList(getMakeUpProductByType(BLUSH_ENDPOINT)));
+        products.add(Arrays.asList(getMakeUpProductByType(EYELINER_ENDPOINT)));
+        products.add(Arrays.asList(getMakeUpProductByType(FOUNDATION_ENDPOINT)));
+        products.add(Arrays.asList(getMakeUpProductByType(LIPSTICK_ENDPOINT)));
+        products.add(Arrays.asList(getMakeUpProductByType(MASCARA_ENDPOINT)));
+
+        // result list
+        List<List<MakeUp>> recommended = new ArrayList<>();
+
+        for (int i = 0; i < products.size(); i++) {
+            // temporary list of MakeUp (blush, eyeliner, foundation, lipstick, mascara)
+            List<MakeUp> temp = new ArrayList<>(products.get(i));
+            // add an empty list of MakeUp
+            recommended.add(new ArrayList<>());
+            for (int j = 0; j < temp.size(); j++) {
+                // temporary MakeUp product
+                MakeUp tempMakeUp = temp.get(j);
+                // pick one product randomly if no tag returned
+                if (tagList[i].isEmpty()) {
+                    Random r = new Random();
+                    recommended.get(i).add(temp.get(r.nextInt(temp.size())));
+                    break;
+                } // if
+                // add to list when description contains tag
+                if (tempMakeUp.description != null) {
+                    if (tempMakeUp.description.contains(tagList[i])) {
+                        recommended.get(i).add(tempMakeUp);
+                    } // if
+                } // if
+            } // for
+        } // for
+        return recommended;
     }
 
-    // Method to recommend makeup based on weather conditions
-    public static String[][] recommendMakeUpTag(String weatherText, int relativeHumidity) {
-        if (weatherText.contains("hot") && relativeHumidity > 60) {
+    /**
+     * Randomly chooses one product from each makeup type list
+     * if there are multiple products available.
+     *
+     * @param productListByType A list of lists containing makeup products
+     *                          categorized by makeup type.
+     *                          Each inner list corresponds to a makeup type and
+     *                          contains makeup products of that type.
+     * @return A list of lists containing randomly chosen makeup products for each makeup type.
+     * Each inner list corresponds to a makeup type and
+     * contains a single randomly chosen makeup product of that type.
+     */
+    private static List<List<MakeUp>> chooseOneProductRandomly(
+        List<List<MakeUp>> productListByType) {
+
+        List<List<MakeUp>> result = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < productListByType.size(); i++) {
+            result.add(new ArrayList<>());
+            List<MakeUp> temp = productListByType.get(i);
+            if (productListByType.get(i).size() > 1) {
+                // add 1 random filtered product to its product_by_type list
+                result.get(i).add(temp.get(r.nextInt(temp.size())));
+            } else if (productListByType.get(i).size() == 1) {
+                result.get(i).add(temp.get(0));
+            } // if
+        } // for
+        return result;
+    }
+
+    /**
+     * Recommends makeup based on weather conditions, temperature, and relative humidity.
+     *
+     * @param weatherText      The weather condition text.
+     * @param temperature      The temperature in Celsius.
+     * @param relativeHumidity The relative humidity percentage.
+     * @return An array of makeup product tags recommended for each makeup type
+     * (blush, eyeliner, foundation, lipstick, mascara).
+     * Each element in the array corresponds to a makeup type and
+     * contains a tag or tags that represent the recommended makeup products.
+     * If no specific recommendation is made for a makeup type,
+     * an empty string is returned for that type.
+     */
+    public static String[] recommendMakeUpTag(
+        String weatherText, int temperature, int relativeHumidity) {
+
+        if ((weatherText.contains("hot") | temperature > 35) && relativeHumidity > 60) {
             // Hot and humid weather
             // Recommend lightweight and long-lasting makeup products such as oil-free foundation,
             // waterproof mascara, and mattifying setting powder to combat sweat and shine.
-            return new String[][]{new String[0], new String[0], new String[] {"oil-free"},
-                    new String[0], new String[]{"water", "waterproof"}};
-        } else if (weatherText.contains("cold") && relativeHumidity > 60) {
+            return new String[]{"", "", "oil-free", "", "waterproof"};
+        } else if ((weatherText.contains("cold") | temperature <= 10) && relativeHumidity > 60) {
             // Cold and humid weather
             // Recommend hydrating foundation formulas, moisturizing lipsticks,
             // and cream blushes to keep the skin hydrated and prevent dryness.
-            return new String[][]{new String[]{"cream"}, new String[0], new String[] {"hydrating"},
-                    new String[]{"moisture", "moisturizing"}, new String[0]};
-        } else if (weatherText.contains("hot") && relativeHumidity < 30) {
+            return new String[]{"cream", "", "hydrating", "moisturizing", ""};
+        } else if ((weatherText.contains("cold") | temperature <= 10) && relativeHumidity < 30) {
             // Hot and dry weather
             // Recommend hydrating and lightweight makeup products to prevent dryness and flakiness.
             // Use a moisturizing primer and dewy finish foundation.
-            return new String[][]{new String[0], new String[0],
-                    new String[]{"hydrating"}, new String[0], new String[0]};
+            return new String[]{"", "", "dewy", "", ""};
         } else if (weatherText.contains("cold") && relativeHumidity < 30) {
             // Cold and dry weather
             // Recommend hydrating foundation formulas, moisturizing lipsticks,
             // and cream blushes to keep the skin hydrated and prevent dryness.
-            return new String[][]{new String[]{"cream"}, new String[0], new String[] {"hydrating"},
-                    new String[]{"moisture", "moisturizing"}, new String[0]};
+            return new String[]{"cream", "", "hydrating", "moisturizing", ""};
         } else if (weatherText.contains("rain") || weatherText.contains("showers")) {
             // Rainy weather
             // Recommend waterproof makeup products such as
             // waterproof eyeliner and mascara, as well as
             // long-lasting lipsticks that withstand rain and humidity.
-            return new String[][]{new String[0], new String[]{"water-resistant", "waterproof"},
-                    new String[0], new String[]{"long"}, new String[]{"water", "waterproof"}};
+            return new String[]{"", "waterproof", "", "long", "waterproof"};
         } else if (weatherText.contains("sunny") && relativeHumidity < 30) {
             // Sunny and dry weather
             // Recommend makeup products with built-in SPF protection,
             // lightweight and breathable formulas to prevent sweat and shine.
-            return new String[][]{new String[0], new String[0],
-                    new String[]{"spf"}, new String[0], new String[0]};
+            return new String[]{"", "", "spf", "", ""};
         } else if ((weatherText.contains("sunny") && relativeHumidity > 60) |
                 (weatherText.contains("windy") && relativeHumidity > 60)) {
             // Sunny and humid weather
             // Recommend lightweight makeup products with mattifying properties
             // to control shine and oiliness. Use a primer to prolong makeup wear.
-            return new String[][]{new String[]{"matte"}, new String[]{"matte"},
-                    new String[]{"matte"}, new String[]{"matte"}, new String[0]};
+            return new String[]{"matte", "matte", "matte", "matte", "matte"};
         } else if (weatherText.contains("windy") && relativeHumidity < 30) {
             // Windy and dry weather
             // Recommend long-lasting and transfer-resistant makeup,
             // including smudge-proof eyeliners, waterproof mascaras,
             // and matte lipsticks. Setting sprays can also help to lock makeup in place.
-            return new String[][]{new String[0], new String[]{"smudger", "smudgeliner"},
-                    new String[0], new String[]{"matte"}, new String[]{"water", "waterproof"}};
+            return new String[]{"", "smudger", "", "matte", "waterproof"};
+        } else {
+            // anything is ok
+            return new String[]{"", "", "", "", ""};
         } // if
-        return new String[0][];
     } // recommendMakeUp
 } // MakeUpAPI
